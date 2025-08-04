739f96217ba27651ff00c8bc71f5137912241cc0
AurinkoHealthCare <aurinkohealthcare2014gmail.com>
2025-08-04 17:50:34 +0530

    run pull

--- frontend/utils/todo.jsx
+++ frontend/utils/todo.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 
 const TodoApp = () => {
-    const [list,setList]=useState([])
-
+const [add,setadd]=useState([]);
     function Add(){
+
+    }
+
+    function handelChang(e){
+        setadd(e.target.value)
     }
   return (
     <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
       <h1 className="text-2xl text-center font-semibold mb-6">To-Do List</h1>
       <div className="flex mb-4">
-        <input 
+        <input
           type="text" 
           placeholder="Add a new todo" 
           className="w-4/5 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={handelChang}
         />
         <button
         onClick={Add} 
